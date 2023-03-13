625b6192859aa380ed946267c0f171877aba1f85
recorror <itgwd9701@gmail.com>
2023-03-13 17:58:26 +0900

    create react app

---
+++ frontend/beacon-of-stock/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-3xl font-bold underline'>
+        hello typescript
+      </p>
+    </div>
+  );
+}
+
+export default App;
