64718f75720991a29623a66991c6b4c830834b71
박희종 <qgw13467@naver.com>
2023-03-18 12:29:41 +0900

    feat(BE): add member fuc

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
@@ -1,4 +1,26 @@
 package com.ssafy.beconofstock.member.controller;
 
+import com.ssafy.beconofstock.member.dto.UserInfoDto;
+import com.ssafy.beconofstock.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("user/{userId}")
+    public ResponseEntity<UserInfoDto> getUserHistoryInfo(@PathVariable("userId")Long userId){
+
+        UserInfoDto userInfoDto = memberService.getUserInfoDto(userId);
+
+
+        return new ResponseEntity<>(userInfoDto,HttpStatus.OK);
+    }
 }
--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/service/MemberService.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/service/MemberService.java
@@ -1,4 +1,8 @@
 package com.ssafy.beconofstock.member.service;
 
+import com.ssafy.beconofstock.member.dto.UserInfoDto;
+
 public interface MemberService {
+    UserInfoDto getUserInfoDto(Long memberId);
+
 }
