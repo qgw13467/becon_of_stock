59fef3b235384557e550e51fecb095537279143b
박희종 <qgw13467@naver.com>
2023-03-19 02:24:27 +0900

    feat(BE): modify url mapping

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;
