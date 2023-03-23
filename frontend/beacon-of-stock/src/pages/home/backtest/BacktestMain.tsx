d56b5c5397cbdb49c12201a3b76e4c2d81b2d603
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-03-23 17:24:37 +0900

    Feat : BasicSetting 유효성 검사 추가
    - basicsetting 유효성 검사 추가
    - css 수정

--- frontend/beacon-of-stock/src/pages/home/backtest/BacktestMain.tsx
+++ frontend/beacon-of-stock/src/pages/home/backtest/BacktestMain.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import BasicSettings from "./BasicSetting/BasicSettings";
-import Factor from "./Factor/Factor";
-import FactorSettings from "./Factor/FactorSettings";
-import SelectedItems from "./Factor/SelectedItems";
+import BasicSettings from './BasicSetting/BasicSettings';
+import Factor from './Factor/Factor';
+import FactorSettings from './Factor/FactorSettings';
+import SelectedItems from './Factor/SelectedItems';
 
 const backTestHandler = (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
@@ -13,26 +13,26 @@ const BacktestMain = () => {
   return (
     <React.Fragment>
       <form onSubmit={backTestHandler}>
-        <div className="flex justify-end my-[1%] mr-[8%]">
+        <div className='flex justify-end my-[1%] mr-[8%]'>
           <input
-            className="text-lg font-KJCbold text-[#FEFEFE] border rounded-xl bg-[#A47ECF] px-[1%] py-[0.5%]"
-            type="submit"
-            value="백테스트"
+            className='text-lg font-KJCbold text-[#FEFEFE] border rounded-xl bg-[#A47ECF] px-[1%] py-[0.5%]'
+            type='submit'
+            value='백테스트'
           />
         </div>
-        <main className="h-[80vh] flex place-content-around items-center mx-[5%]">
-          <section className="relative inline-block w-[29%] h-full border">
+        <main className='h-[80vh] flex place-content-around items-center mx-[5%]'>
+          <section className='relative inline-block w-[29%] h-full border'>
             <BasicSettings />
           </section>
-          {/* <section className="relative inline-block w-[63%] h-full border">
+          <section className='relative inline-block w-[63%] h-full border'>
             <Factor />
-          </section> */}
-          <section className="relative inline-block w-[29%] h-full border">
+          </section>
+          {/* <section className="relative inline-block w-[29%] h-full border">
             <FactorSettings />
           </section>
           <section className="relative inline-block w-[29%] h-full border">
             <SelectedItems />
-          </section>
+          </section> */}
         </main>
       </form>
     </React.Fragment>
--- frontend/beacon-of-stock/src/pages/home/backtest/Factor/Factor.tsx
+++ frontend/beacon-of-stock/src/pages/home/backtest/Factor/Factor.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import FactorSettings from "./FactorSettings";
-import SelectedItems from "./SelectedItems";
+import React from 'react';
+import FactorSettings from './FactorSettings';
+import SelectedItems from './SelectedItems';
 
 const Factor = () => {
   return (
-    <main className="flex items-center place-content-between">
-      <section className="relative inline-block w-[46.1%] h-full border">
+    <main className='flex items-center h-full place-content-between'>
+      <section className='relative inline-block w-[46.1%] h-full border'>
         <FactorSettings />
       </section>
-      <section className="relative inline-block w-[46.1%] h-full border">
+      <section className='relative inline-block w-[46.1%] h-full border'>
         <SelectedItems />
       </section>
     </main>
