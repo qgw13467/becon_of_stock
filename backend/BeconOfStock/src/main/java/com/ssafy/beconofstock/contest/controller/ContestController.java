6b4520666d11896d0bcfe087ac2edc644e1d74ba
gyocheol <kys04171@naver.com>
2023-03-23 17:56:45 +0900

    Feat(BE) : Contest API 개발중

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/contest/controller/ContestController.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/contest/controller/ContestController.java
@@ -1,11 +1,19 @@
 package com.ssafy.beconofstock.contest.controller;
 
+import com.ssafy.beconofstock.contest.dto.ContestResponseDto;
+import com.ssafy.beconofstock.contest.entity.Contest;
 import com.ssafy.beconofstock.contest.service.ContestService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Api(tags = {"Contest"})
 @Controller
@@ -16,5 +24,10 @@ public class ContestController {
 
     private final ContestService contestService;
 
-    
+    @GetMapping
+    public ResponseEntity<?> getContests() {
+        List<ContestResponseDto> contestList = contestService.getContestList();
+
+        return new ResponseEntity<>(contestList, HttpStatus.CREATED);
+    }
 }
