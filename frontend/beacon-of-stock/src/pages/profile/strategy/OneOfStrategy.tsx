e8f7411e033ba6c42b4ce1c0f74d519eb49921a3
recorror <itgwd9701@gmail.com>
2023-03-23 17:57:54 +0900

    Feat : Strategy 기본 폼 + 페이징 진행 중
    - prev, next 먹여야함.

--- frontend/beacon-of-stock/src/pages/profile/strategy/OneOfStrategy.tsx
+++ frontend/beacon-of-stock/src/pages/profile/strategy/OneOfStrategy.tsx
@@ -6,8 +6,8 @@ type OneOfStrategyProps = {
 
 export const OneOfStrategy = ({ item }: OneOfStrategyProps) => {
   return (
-    <div className='relative w-[240px] h-[180px] border-black rounded-md border-2 m-auto my-2'>
-      <div className='absolute border-black border-2 bg-[#808080] w-[240px] h-[80px] rounded-b-md -bottom-[9px] my-2 -right-[1.5px] m-auto'></div>
+    <div className='relative w-[240px] h-[180px] border-[#7D8AD8] rounded-md border-2 m-auto my-2'>
+      <div className='absolute border-[#7D8AD8] border-2 bg-[#5598DE] text-[#fefefe] w-[240px] h-[80px] rounded-b-md -bottom-[9px] my-2 -right-[1.5px] m-auto'></div>
     </div>
   );
 };
