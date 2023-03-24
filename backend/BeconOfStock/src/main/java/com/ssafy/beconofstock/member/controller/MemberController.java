0860ae8b9d9a4e0c944612c37af1cab4ff276a72
임지원 <wldnjs4255@naver.com>
2023-03-24 13:25:42 +0900

    feat(BE): add swagger to membercontroller
    스웨거 명세 작성

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/member/controller/MemberController.java
@@ -4,8 +4,7 @@ import com.ssafy.beconofstock.authentication.user.OAuth2UserImpl;
 import com.ssafy.beconofstock.member.dto.FollowedDto;
 import com.ssafy.beconofstock.member.dto.UserInfoDto;
 import com.ssafy.beconofstock.member.service.MemberService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,29 +24,39 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @ApiOperation(value = "회원 정보 조회",  notes =
+    @ApiOperation(value = "회원 정보 조회", notes =
             "회원 정보를 조회합니다.")
     @GetMapping("user/{userId}")
-    public ResponseEntity<UserInfoDto> getUserHistoryInfo(@PathVariable("userId")Long userId){
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공입니다."),
+    })
+    public ResponseEntity<UserInfoDto> getUserHistoryInfo(@PathVariable("userId") @ApiParam(value = "조회할 회원의 아이디") Long userId) {
 
         UserInfoDto userInfoDto = memberService.getUserInfoDto(userId);
 
-        return new ResponseEntity<>(userInfoDto,HttpStatus.OK);
+        return new ResponseEntity<>(userInfoDto, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "회원 정보 수정",  notes =
+    @ApiOperation(value = "회원 정보 수정", notes =
             "회원 정보를 수정합니다.")
     @PutMapping("user")
-    public ResponseEntity<UserInfoDto> updateUserInfo(@RequestBody UserInfoDto userInfoDto, @AuthenticationPrincipal OAuth2UserImpl oAuth2User){
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "성공입니다."),
+    })
+    public ResponseEntity<UserInfoDto> updateUserInfo(@RequestBody @ApiParam(value = "수정할 회원 정보(닉네임)") UserInfoDto userInfoDto, @AuthenticationPrincipal OAuth2UserImpl oAuth2User) {
 
         memberService.updateUserInfo(oAuth2User.getMember(), userInfoDto);
 
         return new ResponseEntity<>(userInfoDto, HttpStatus.CREATED);
     }
-    @ApiOperation(value = "회원 탈퇴",  notes =
+
+    @ApiOperation(value = "회원 탈퇴", notes =
             "회원을 탈퇴합니다.")
     @DeleteMapping("user")
-    public ResponseEntity<UserInfoDto> deleteUser(@AuthenticationPrincipal OAuth2UserImpl oAuth2User){
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공입니다."),
+    })
+    public ResponseEntity<UserInfoDto> deleteUser(@AuthenticationPrincipal OAuth2UserImpl oAuth2User) {
 
         memberService.deleteUser(oAuth2User.getMember());
 
@@ -55,30 +64,40 @@ public class MemberController {
     }
 
 
-    @ApiOperation(value = "팔로우 목록 조회",  notes =
+    @ApiOperation(value = "팔로우 목록 조회", notes =
             "팔로우 목록을 조회합니다.")
     @GetMapping("/follows")
-    public ResponseEntity<?> getFollows(@AuthenticationPrincipal OAuth2UserImpl oAuth2User){
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공입니다."),
+    })
+    public ResponseEntity<?> getFollows(@AuthenticationPrincipal OAuth2UserImpl oAuth2User) {
 
         List<FollowedDto> follows = memberService.getFollows(oAuth2User.getMember());
 
-        return new ResponseEntity<>(follows,HttpStatus.OK);
+        return new ResponseEntity<>(follows, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "팔로우 대상 추가", notes=
+    @ApiOperation(value = "팔로우 대상 추가", notes =
             "팔로우 대상을 추가합니다.")
     @PostMapping("/follows/{userId}")
-    public ResponseEntity<?> saveFollow(@PathVariable("userId") long userId, @AuthenticationPrincipal OAuth2UserImpl oAuth2User){
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공입니다."),
+    })
+    public ResponseEntity<?> saveFollow(@PathVariable("userId")  @ApiParam(value = "팔로우할 회원의 아이디") long userId, @AuthenticationPrincipal OAuth2UserImpl oAuth2User) {
 
         memberService.saveFollow(oAuth2User.getMember(), userId);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @ApiOperation(value = "팔로우 대상 삭제", notes=
+
+    @ApiOperation(value = "팔로우 대상 삭제", notes =
             "팔로우 대상을 삭제합니다.")
     @DeleteMapping("/follows/{userId}")
-    public ResponseEntity<?> deleteFollow(@PathVariable("userId") long userId, @AuthenticationPrincipal OAuth2UserImpl oAuth2User){
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공입니다."),
+    })
+    public ResponseEntity<?> deleteFollow(@PathVariable("userId")  @ApiParam(value = "팔로우를 취소할 회원의 아이디") long userId, @AuthenticationPrincipal OAuth2UserImpl oAuth2User) {
 
         memberService.deleteFollow(oAuth2User.getMember(), userId);
 
