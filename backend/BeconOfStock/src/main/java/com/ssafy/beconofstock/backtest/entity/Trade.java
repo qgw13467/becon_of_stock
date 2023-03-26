b995fb784f415959395365e7401ffaeeccf53074
박희종 <qgw13467@naver.com>
2023-03-27 02:07:55 +0900

    feat(BE): modify entity

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/entity/Trade.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/entity/Trade.java
@@ -1,13 +1,21 @@
 package com.ssafy.beconofstock.backtest.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class Trade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Finance finance;
 
     private String corcode;
     private String corname;
