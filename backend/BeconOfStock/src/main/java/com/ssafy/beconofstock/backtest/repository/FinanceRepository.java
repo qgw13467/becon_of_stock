0565fd9c08e47dc61ea14e8c336176c34d882525
박희종 <qgw13467@naver.com>
2023-03-27 02:08:42 +0900

    feat(BE): modify Finance, Trade repository

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/FinanceRepository.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/FinanceRepository.java
@@ -4,6 +4,10 @@ import com.ssafy.beconofstock.backtest.entity.Finance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FinanceRepository extends JpaRepository<Finance,Long> {
+
+    Optional<Finance> findByRptYearAndRptMonthAndCorName(Integer year, Integer month, String corName);
 }
