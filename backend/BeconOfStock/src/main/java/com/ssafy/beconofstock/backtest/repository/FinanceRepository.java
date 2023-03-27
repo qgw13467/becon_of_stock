37221c44620a74c7245cc797d43df202a59f1852
박희종 <qgw13467@naver.com>
2023-03-27 20:18:20 +0900

    feat(BE): add Finanace, Trade repository method

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/FinanceRepository.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/FinanceRepository.java
@@ -2,12 +2,18 @@ package com.ssafy.beconofstock.backtest.repository;
 
 import com.ssafy.beconofstock.backtest.entity.Finance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FinanceRepository extends JpaRepository<Finance,Long> {
 
     Optional<Finance> findByRptYearAndRptMonthAndCorName(Integer year, Integer month, String corName);
+    List<Finance> findByRptYearAndRptMonth(Integer year, Integer month);
+    @Query("SELECT f FROM Finance f WHERE f.rptYear = :year AND f.rptMonth IN (:months)")
+    List<Finance> findByRptYearAndRptMonth(@Param("year") Integer year,@Param("months") List<Integer> months);
 }
--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/TradeRepository.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/repository/TradeRepository.java
@@ -3,15 +3,16 @@ package com.ssafy.beconofstock.backtest.repository;
 import com.ssafy.beconofstock.backtest.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TrateRepository extends JpaRepository<Trade, Long> {
+public interface TradeRepository extends JpaRepository<Trade, Long> {
 
     @Query("SELECT tr FROM Trade tr LEFT JOIN FETCH tr.finance WHERE tr.year = :year AND tr.month =:month")
-    List<Trade> findByYearAndMonthFetch(Integer year, Integer month);
+    List<Trade> findByYearAndMonthFetch(@Param("year") Integer year, @Param("month") Integer month);
 
     List<Trade> findByYearAndMonth(Integer year, Integer month);
 }
