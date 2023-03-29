97ceff3d003610d327a5ebaf4aa0daef81dc6448
recorror <itgwd9701@gmail.com>
2023-03-29 11:03:32 +0900

    Feat : bookmark#2
    - hang over 시 애니메이션 추가

--- frontend/beacon-of-stock/src/component/nav/ProfileBox.tsx
+++ frontend/beacon-of-stock/src/component/nav/ProfileBox.tsx
@@ -9,24 +9,24 @@ export const ProfileBox = () => {
     navigate('/');
   };
 
+  const profileStyle =
+    'text-center inline cursor-pointer w-[106px] p-1 rounded-lg hover:bg-[#1B4978] hover:text-[#fefefe]';
+  const plusColor = `text-center inline cursor-pointer w-[106px] p-1 rounded-lg hover:bg-[#BA4C85] hover:text-[#fefefe] text-[#DE2727]`;
   return (
     <div
-      className='absolute border-2 border-black w-[150px] h-[180px] right-6 top-16 grid content-around bg-[#fefefe] rounded-lg z-50'
+      className='absolute border-2 border-black w-[150px] h-[180px] right-6 top-16 grid justify-center content-around bg-[#fefefe] rounded-lg z-50'
       // onClick={modalCloseHandler}
     >
-      <Link to='/myProfile' className='text-center inline cursor-pointer'>
+      <Link to='/myProfile' className={profileStyle}>
         내 정보
       </Link>
-      <Link to='/strategy' className='text-center inline cursor-pointer'>
+      <Link to='/strategy' className={profileStyle}>
         내 전략조회
       </Link>
-      <Link to='/bookmark' className='text-center cursor-pointer'>
+      <Link to='/bookmark' className={profileStyle}>
         북마크
       </Link>
-      <p
-        className='text-center text-[#DE2727] inline cursor-pointer'
-        onClick={() => handleLogout()}
-      >
+      <p className={plusColor} onClick={() => handleLogout()}>
         로그아웃
       </p>
     </div>
--- frontend/beacon-of-stock/src/pages/profile/bookmark/Bookmark.tsx
+++ frontend/beacon-of-stock/src/pages/profile/bookmark/Bookmark.tsx
@@ -1,3 +1,36 @@
+import { SearchbarNone } from '../../../component/search/SearchbarNone';
+import { Pagenation } from '../../../component/Pagenation';
+import { TileBoard } from '../../../component/TileBoard';
+import StrategySelect from '../../../component/select_box/StrategySelect';
+
 export const Bookmark = () => {
-  return <div>북마크 페이지입니다.</div>;
+  const PAGE_SIZE = 20; // 한 페이지에 보여질 게시글 수
+  const 게시물수 = 10000; // back에서 총 게시물을 받아오게 할 것인가? x
+  // 총 페이지 수
+  const pageEA = Math.floor(게시물수 / PAGE_SIZE);
+  // 더미
+  const items: number[] = Array(PAGE_SIZE).fill(0);
+  // console.log(pageEA);
+
+  return (
+    <section>
+      <p className='font-KJCbold text-4xl m-9'>북마크</p>
+      {/* 필터링 부분 */}
+      <article className='flex justify-between'>
+        <StrategySelect />
+        <SearchbarNone />
+      </article>
+      <article className='grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 content-evenly mx-32'>
+        {items.map((item, index) => (
+          <TileBoard key={index} item={item} />
+        ))}
+      </article>
+      <article className='my-8'>
+        <Pagenation pageEA={pageEA} />
+      </article>
+      <article className='flex justify-center ml-32 my-8'>
+        <SearchbarNone />
+      </article>
+    </section>
+  );
 };
