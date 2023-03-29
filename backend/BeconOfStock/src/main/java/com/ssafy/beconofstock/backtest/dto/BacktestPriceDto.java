cfbc3c0480842b26dde8977aac9ab5a4a988c0de
박희종 <qgw13467@naver.com>
2023-03-29 13:13:43 +0900

    feat(BE): add anotation, discription

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestPriceDto.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestPriceDto.java
@@ -1,11 +1,15 @@
 package com.ssafy.beconofstock.backtest.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BacktestPriceDto {
 
     private Integer year;
--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestResultDto.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestResultDto.java
@@ -11,21 +11,35 @@ public class BacktestResultDto {
 
     List<BacktestPriceDto> strategyValues;
     List<BacktestPriceDto> marketValues;
+    //누적 수익률
     private Double strategyCumulativeReturn;
     private Double marketCumulativeReturn;
+
+    //기간별 평균 수익률
     private Double strategyCagr;
     private Double marketCagr;
+
     private Double strategySharpe;
     private Double margetSharpe;
     private Double strategySortino;
     private Double marketSortino;
+
+    //상관계수
     private Double correlation;
+
+    //최대 하락폭
     private Double strategyMDD;
     private Double marketMDD;
+
+    //각 기간(연도, 달)별 수익률
     List<ChangeRateDto> changeRate;
+
+    //수익을 만든 횟수
     private Integer strategyRevenue;
     private Integer marketRevenue;
     private Integer totalMonth;
+
+    //주기별 수익률 평균
     private Double strategyRevenueAvg;
     private Double marketRevenueAvg;
 
