ceb4a061da83a83a78ed3fdbdfa4ff52125a262a
임유정 <melimelame@gmail.com>
2023-03-29 15:11:25 +0900

    feat(BE): add strategy on Board
    - 게시판 전략 포함

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/board/dto/BoardResponseDto.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/board/dto/BoardResponseDto.java
@@ -1,16 +1,18 @@
 package com.ssafy.beconofstock.board.dto;
 
 import com.ssafy.beconofstock.board.entity.Board;
+import com.ssafy.beconofstock.strategy.dto.StrategyDetailDto;
+import com.ssafy.beconofstock.strategy.entity.Indicator;
 import com.ssafy.beconofstock.strategy.entity.Strategy;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class BoardResponseDto {
-
     @ApiModelProperty(example = "게시글 번호")
     Long boardId;
     @ApiModelProperty(example = "작성자 닉네임")
@@ -24,7 +26,7 @@ public class BoardResponseDto {
     @ApiModelProperty(example = "글 내용")
     String content;
     @ApiModelProperty(example = "전략")
-    Strategy strategy;
+    StrategyDetailDto strategy;
     @ApiModelProperty(example = "댓글 수")
     Long commentNum;
     @ApiModelProperty(example = "좋아요 수")
@@ -34,6 +36,22 @@ public class BoardResponseDto {
     @ApiModelProperty(example = "찜하기 상태")
     Boolean dibStatus;
 
+    public BoardResponseDto(Board board, List<Indicator> indicators, Boolean likeStatus, Boolean dibStatus) {
+        this.boardId = board.getId();
+        this.createDate = board.getCreatedDateTime();
+        this.nickname = board.getMember().getNickname();
+        this.hit = board.getHit();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        if (board.getStrategy() != null) {
+            this.strategy = strategyDetailDto(board.getStrategy(), indicators);
+        }
+        this.likeNum = board.getLikeNum();
+        this.commentNum = board.getCommentNum();
+        this.likeStatus = likeStatus;
+        this.dibStatus = dibStatus;
+    }
+
     public BoardResponseDto(Board board, Boolean likeStatus, Boolean dibStatus) {
         this.boardId = board.getId();
         this.createDate = board.getCreatedDateTime();
@@ -41,10 +59,24 @@ public class BoardResponseDto {
         this.hit = board.getHit();
         this.title = board.getTitle();
         this.content = board.getContent();
-        this.strategy = board.getStrategy();
         this.likeNum = board.getLikeNum();
         this.commentNum = board.getCommentNum();
         this.likeStatus = likeStatus;
         this.dibStatus = dibStatus;
+    }
+
+    public StrategyDetailDto strategyDetailDto(Strategy strategy, List<Indicator> indicators) {
+
+        return  StrategyDetailDto.builder()
+                .id(strategy.getId())
+                .title(strategy.getTitle())
+                .sharpe(strategy.getSharpe())
+                .cagr(strategy.getCagr())
+                .cumulativeReturn(strategy.getCumulativeReturn())
+                .memberNickname(strategy.getMember().getNickname())
+                .memberId((strategy.getMember().getId()))
+                .indicators(indicators)
+                .access(strategy.getAccessType())
+                .build();
     }
 }
