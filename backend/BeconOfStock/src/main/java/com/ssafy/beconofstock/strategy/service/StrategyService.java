b9ccf5ecf31c54e85e70f38b76042715a4f83482
임지원 <wldnjs4255@naver.com>
2023-03-30 02:53:53 +0900

    feat(BE): add getIndustries method
    getIndustries 메서드 추가

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/strategy/service/StrategyService.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/strategy/service/StrategyService.java
@@ -2,6 +2,7 @@ package com.ssafy.beconofstock.strategy.service;
 
 import com.ssafy.beconofstock.member.entity.Member;
 import com.ssafy.beconofstock.strategy.dto.IndicatorsDto;
+import com.ssafy.beconofstock.strategy.dto.IndustriesDto;
 import com.ssafy.beconofstock.strategy.dto.StrategyAddDto;
 import com.ssafy.beconofstock.strategy.dto.StrategyDetailDto;
 import com.ssafy.beconofstock.strategy.entity.StrategyIndicator;
@@ -13,9 +14,10 @@ public interface StrategyService {
 
     StrategyDetailDto getStrategyDetail(Member member, Long strategyId);
     IndicatorsDto getIndicators();
+    IndustriesDto getIndustries();
     List<StrategyIndicator> getStrategy(Long id);
     void addStrategy(Member member, StrategyAddDto strategyAddDto);
     void patchStrategy(Member member, StrategyAddDto strategyAddDto, Long strategyId);
-    void deleteStrategy(Member member, Long StrategyId);
 
+    void deleteStrategy(Member member, Long StrategyId);
 }
