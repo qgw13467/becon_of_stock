a1706148accbc6e057352059423d5835c943fbf5
박희종 <qgw13467@naver.com>
2023-03-31 01:31:50 +0900

    feat(BE): modify SecurityConfig

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/config/SecurityConfig.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/config/SecurityConfig.java
@@ -40,7 +40,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/backtest").permitAll()
                 .antMatchers("/login/oauth2/code/kakao").permitAll()
                 .antMatchers("/favicon.ico").permitAll()
                 .antMatchers(
