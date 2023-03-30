bac2b4ac74c667aff49f1168850df59c85d2ee1e
박희종 <qgw13467@naver.com>
2023-03-30 22:26:16 +0900

    feat(BE): add backtest controller

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestIndicatorsDto.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/BacktestIndicatorsDto.java
@@ -3,11 +3,13 @@ package com.ssafy.beconofstock.backtest.dto;
 import com.ssafy.beconofstock.backtest.entity.BacktestSortType;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class BacktestIndicatorsDto {
     List<Long> indicators;
     List<Long> industries;
