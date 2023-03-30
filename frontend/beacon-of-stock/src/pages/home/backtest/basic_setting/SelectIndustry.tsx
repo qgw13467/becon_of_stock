dfb081fe834ab0e7043b60ed5dc29c001e79f535
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-03-30 10:13:54 +0900

    Feat : 산업 선택 틀 구현
    - 산업군이 추가될 경우 틀 수정 필요

--- frontend/beacon-of-stock/src/pages/home/backtest/basic_setting/SelectIndustry.tsx
+++ frontend/beacon-of-stock/src/pages/home/backtest/basic_setting/SelectIndustry.tsx
@@ -1,12 +1,37 @@
+import React, { useState } from 'react';
+import checkboxChecked from '../../../../assets/img/checkbox-checked.png';
+import checkboxBlank from '../../../../assets/img/checkbox-blank.png';
+
 interface Props {
   id: number;
   industryName: string;
 }
 
 const SelectIndustry = (props: Props) => {
+  // 산업 선택 여부
+  const [industrySelected, setIndustrySelected] = useState<boolean>(true);
+  const industrySelectedHandler = () => {
+    setIndustrySelected(!industrySelected);
+  };
+
   return (
-    <div>
-      <div>{props.industryName}</div>
+    <div className='flex items-center my-[2%] ml-[5%]'>
+      {industrySelected ? (
+        <img
+          src={checkboxChecked}
+          alt='checkboxChecked'
+          className='w-4 h-4'
+          onClick={industrySelectedHandler}
+        />
+      ) : (
+        <img
+          src={checkboxBlank}
+          alt='checkboxBlank'
+          className='w-4 h-4'
+          onClick={industrySelectedHandler}
+        />
+      )}
+      <div className='ml-2 text-lg'>{props.industryName}</div>
     </div>
   );
 };
