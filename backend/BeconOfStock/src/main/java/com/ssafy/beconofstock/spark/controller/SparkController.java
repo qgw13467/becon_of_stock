91af0d9e836f83d230916e119e25484eb773cb00
임지원 <wldnjs4255@naver.com>
2023-04-02 00:17:51 +0900

    feat(BE): add sparkController
    add sparkController

---
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/spark/controller/SparkController.java
@@ -0,0 +1,41 @@
+package com.ssafy.beconofstock.spark.controller;
+
+import com.ssafy.beconofstock.backtest.dto.BacktestIndicatorsDto;
+import com.ssafy.beconofstock.spark.dto.TestDto;
+import com.ssafy.beconofstock.spark.service.SparkService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Api(tags = {"Spark 관련 API"})
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Slf4j
+public class SparkController {
+
+    private final SparkService sparkService;
+
+    @ApiOperation(value = "스파크 테스트", notes = "trade 테이블 개수를 리턴합니다.")
+    @GetMapping("/spark/test")
+    @ApiResponses({@ApiResponse(code = 200, message = "성공입니다."),})
+    public ResponseEntity<TestDto> doSparkTest(
+            @ModelAttribute BacktestIndicatorsDto backtestIndicatorsDto) {
+
+        log.info("========= doSparkTest : {} ", backtestIndicatorsDto.toString());
+
+        TestDto testDto = sparkService.getConnectionTest();
+
+        return new ResponseEntity<>(testDto, HttpStatus.OK);
+    }
+
+}
