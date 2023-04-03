7382198e3992495e2e8805190292fdbe631de27d
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-04-03 21:47:02 +0900

    Feat : backtest
    - 기본 설정 시작 시점과 종료 시점 조건 확인
    - 산업 항목 store 수정

--- frontend/beacon-of-stock/src/store/store.tsx
+++ frontend/beacon-of-stock/src/store/store.tsx
@@ -81,7 +81,9 @@ type backtestIndustryStore = {
 // set: mutate the state
 export const useBacktestIndustryStore = create<backtestIndustryStore>(
   (set) => ({
-    selectedIndustries: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    selectedIndustries: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
     addSelectedIndustry(id: number) {
       set((state) => ({
         ...state,
