e8019a9b3ee59a68269b398ac43d80481b25e55f
박희종 <qgw13467@naver.com>
2023-04-04 13:47:02 +0900

    refactor(BE): modify CumulativeReturnDataDto

--- backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/CumulativeReturnDataDto.java
+++ backend/BeconOfStock/src/main/java/com/ssafy/beconofstock/backtest/dto/CumulativeReturnDataDto.java
@@ -9,19 +9,23 @@ import lombok.*;
 @Builder
 public class CumulativeReturnDataDto {
 
+    private final String culmulativeReturn = "누적 수익률";
     private final String cumulativeReturnDesc = "전략의 누적 수익률";
     private Double strategyCumulativeReturn;
     private Double marketCumulativeReturn;
 
     //기간별 평균 수익률
+    private final String cagr ="구간별 평균 수익률";
     private final  String cagrDesc = "리벨런싱 구간 평균 수익률";
     private Double strategyCagr;
     private Double marketCagr;
 
-    private final  String sharpe = "변동폭을 확인하는 지표";
+    private final String sharpe = "sharpe";
+    private final  String sharpeDesc = "변동폭을 확인하는 지표";
     private Double strategySharpe;
     private Double marketSharpe;
-    private final String sortino = "하락에 대한 변동폭을 확인하는 지표";
+    private final String sortino = "sortino";
+    private final String sortinoDesc = "하락에 대한 변동폭을 확인하는 지표";
     private Double strategySortino;
     private Double marketSortino;
 
@@ -29,7 +33,9 @@ public class CumulativeReturnDataDto {
 //    private Double correlation;
 
     //최대 하락폭
-    private final String mdd = "최대 하락폭을 확인하는 지표";
+
+    private final String mdd = "MDD";
+    private final String mddDesc = "최대 하락폭을 확인하는 지표";
     private Double strategyMDD;
     private Double marketMDD;
 
