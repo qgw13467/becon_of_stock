cb303687603614b44e5a307ae77c3d4301c98e1a
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-04-04 15:43:21 +0900

    Feat : backtestResult
    - 전월대비 증감률 표 추가

--- frontend/beacon-of-stock/src/pages/home/backtest/result/ResultChangeRateTable.tsx
+++ frontend/beacon-of-stock/src/pages/home/backtest/result/ResultChangeRateTable.tsx
@@ -10,53 +10,68 @@ interface Props {
 const ResultChangeRateTable = (props: Props) => {
   return (
     <div className='flex flex-col justify-center'>
-      <p className='text-xl font-KJCbold'>ResultChangeRateTable</p>
+      {/* <p className='text-xl font-KJCbold'>ResultChangeRateTable</p> */}
       {/* 표를 나타내는 태그 */}
       <table className='border'>
         {/* 제목 행을 그룹화하는 태그 */}
         <thead className='border'>
-          <tr className='border'>
-            <th>Month</th>
-            <th>Savings</th>
-            <th>Savings</th>
+          <tr>
+            <th className='border'>구분</th>
+            <th className='border'>시장</th>
+            <th className='border'>내 전략</th>
           </tr>
         </thead>
         {/* 본문 행을 그룹화하는 태그 */}
         <tbody className='border'>
           <tr>
-            <td>January</td>
-            <td>$100</td>
-            <td>$100</td>
+            <td className='flex items-center px-2 py-1'>
+              <div>수익이 있는 달</div>
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {props.revenueDataDto.marketRevenue}
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {props.revenueDataDto.strategyRevenue}
+            </td>
           </tr>
         </tbody>
         <tbody className='border'>
           <tr>
-            <td>January</td>
-            <td>$100</td>
-            <td>$100</td>
+            <td className='flex items-center px-2 py-1'>
+              <div>전체 달</div>
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {props.revenueDataDto.totalMonth}
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {props.revenueDataDto.totalMonth}
+            </td>
           </tr>
         </tbody>
         <tbody className='border'>
           <tr>
-            <td>January</td>
-            <td>$100</td>
-            <td>$100</td>
+            <td className='flex items-center px-2 py-1'>
+              <div>승률</div>
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {(
+                (props.revenueDataDto.marketRevenue * 100) /
+                props.revenueDataDto.totalMonth
+              ).toFixed(2)}
+              %
+            </td>
+            <td className='px-2 py-1 text-right border'>
+              {(
+                (props.revenueDataDto.strategyRevenue * 100) /
+                props.revenueDataDto.totalMonth
+              ).toFixed(2)}
+              %
+            </td>
           </tr>
         </tbody>
         {/* 바닥 행을 그룹화하는 태그 */}
-        <tfoot className='border'>
-          <tr>
-            <td>Sum</td>
-            <td>$180</td>
-            <td>$180</td>
-          </tr>
-        </tfoot>
-        <tfoot className='border'>
-          <tr>
-            <td>Sum</td>
-            <td>$180</td>
-            <td>$180</td>
-          </tr>
+        <tfoot>
+          <tr></tr>
         </tfoot>
       </table>
     </div>
